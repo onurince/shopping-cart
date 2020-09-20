76534ab1c29f4f80969275cf72b322dbd26bd475
onurince <onur.ince@yahoo.com>
2020-09-20 16:20:30 +0300

    Discount strategy check add for zero and negative values

--- src/Discount/Strategy/FlatRateDiscountStrategy.ts
+++ src/Discount/Strategy/FlatRateDiscountStrategy.ts
@@ -2,6 +2,9 @@ import DiscountStrategy from './DiscountStrategy';
 
 class FlatRateDiscountStrategy implements DiscountStrategy {
   public discount(totalPrice: number, discountQuantity: number): number {
+    if (discountQuantity <= 0) {
+      return 0;
+    }
     return discountQuantity;
   }
 }
--- src/Discount/Strategy/PercentageDiscountStrategy.ts
+++ src/Discount/Strategy/PercentageDiscountStrategy.ts
@@ -2,6 +2,9 @@ import DiscountStrategy from './DiscountStrategy';
 
 class PercentageDiscountStrategy implements DiscountStrategy {
   public discount(totalPrice: number, discountQuantity: number): number {
+    if (discountQuantity <= 0) {
+      return 0;
+    }
     return totalPrice * (discountQuantity / 100);
   }
 }
